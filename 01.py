c54e533268a80f8565573d2edf28cdcc769c19e9
Sammy Fox <foxs11@xavier.edu>
2023-12-01 15:56:05 -0500

    completed day 1

---
+++ 01.py
@@ -0,0 +1,67 @@
+import re
+from  word2number import w2n
+
+
+# f = open('input.txt', 'r')
+
+# input = f.readlines()
+
+# numbers = []
+
+# for i in input :
+#     match = re.search(r'\d', i)
+#     print(match.group())
+
+#     match2 = re.search(r'\d', i[::-1])
+#     print(match2.group())
+
+#     number = int(match.group() + match2.group())
+#     # print(number)
+
+#     numbers.append(number)
+
+# number = 0
+
+# for i in numbers:
+#     number = number + i
+
+# print(number)
+# f.close
+
+
+number_words = {
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"
+    }
+
+f = open('input.txt', 'r')
+
+input = f.readlines()
+
+numbers = []
+
+for i in input :
+    n = i
+    print(n)
+    for number_word in number_words:
+        if number_word in n:
+            n = n.replace(number_word, number_word[0] + str(w2n.word_to_num(number_word)) + number_word[-1])
+    print(n)
+    match = re.search(r'\d', n)
+    print(match.group())
+
+    match2 = re.search(r'\d', n[::-1])
+    print(match2.group())
+
+    number = int(match.group() + match2.group())
+    # print(number)
+
+    numbers.append(number)
+
+number = 0
+
+for i in numbers:
+    number = number + i
+
+print(number)
+f.close
